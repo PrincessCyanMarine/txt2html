367fd6ea2513eeed56fb9411f8dff82483f56547
CyanMarine <rawanrios@gmail.com>
2021-11-17 20:48:16 -0300

    a

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const { readFileSync, writeFileSync, existsSync } = require('fs');
+const { readFileSync, writeFileSync, existsSync, WriteStream } = require('fs');
 const error = () => {
     console.error('Something is wrong, please check https://github.com/PrincessCyanMarine/txt2html#how-to-installuse to see how to isntall correctly\n');
     process.exit(1);
 }
 
-['./config.js', './files/input.txt', './github-markdown.css'].forEach(a => { if (!existsSync(a)) error(); });
+['./config.js', './files/input.txt'].forEach(a => { if (!existsSync(a)) error(); });
 
 
 const { html_text, formatter, github_auth } = require('./config');
@@ -15,19 +15,28 @@ console.log("Read config!");
 const { Octokit } = require("@octokit/core");
 const octokit = new Octokit({ auth: github_auth });
 
-
-
 const input = readFileSync('./files/input.txt', 'utf-8');
 console.log("Read input!");
 const text = formatter(input);
 
 writeFileSync('./files/output.md', text);
 console.log('Written output.md!');
-octokit.request('POST /markdown', { text }).then(body => {
-    const css = readFileSync('./github-markdown.css', 'utf-8');
-    writeFileSync('./files/output.html', html_text(css, body.data));
-    console.log('Written output.html!');
 
-    console.log('\nMade by CyanMarine :P');
-    console.log('Check https://github.com/PrincessCyanMarine/txt2html for more info\n');
-});
+const https = require('https');
+var css = '';
+const request = https.get("https://github.com/sindresorhus/github-markdown-css/blob/main/github-markdown.css", (res) => {
+    res.on('data', (chunk) => { css += chunk });
+    res.on('end', () => {
+        octokit.request('POST /markdown', { text }).then(body => {
+            writeFileSync('./files/output.html', html_text(css, body.data));
+            console.log('Written output.html!');
+
+            console.log('\nMade by CyanMarine :P');
+            console.log('Check https://github.com/PrincessCyanMarine/txt2html for more info\n');
+        });
+
+    });
+})
+
+
+
