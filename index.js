5a121f0d3316da694ba06ce4050334d68454cca9
CyanMarine <rawanrios@gmail.com>
2021-11-17 20:51:10 -0300

    a

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { readFileSync, writeFileSync, existsSync, WriteStream } = require('fs');
-const error = () => {
-    console.error('Something is wrong, please check https://github.com/PrincessCyanMarine/txt2html#how-to-installuse to see how to isntall correctly\n');
+const error = (a) => {
+    console.error(a + '\nPlease check https://github.com/PrincessCyanMarine/txt2html#how-to-installuse to see how to isntall correctly\n');
     process.exit(1);
 }
 
-['./config.js', './files/input.txt'].forEach(a => { if (!existsSync(a)) error(); });
+['./config.js', './files/input.txt'].forEach(a => { if (!existsSync(a)) error(a + ' does not exist'); });
 
 
 const { html_text, formatter, github_auth } = require('./config');
-if (!(html_text && formatter && github_auth) || !(github_auth.trim()) || github_auth == '') error();
+if (!(html_text && formatter && github_auth) || !(github_auth.trim()) || github_auth == '') error("config.js is incorrect");
 console.log("Read config!");
 
 
